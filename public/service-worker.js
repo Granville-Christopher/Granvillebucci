8feb7a6f7610d299911da9363410d0cebd172bb4
Granville Christopher <christophergranville2@gmail.com>
2025-06-09 00:21:05 -0700

    d

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,45 @@
-const CACHE_NAME = "bucci-site-cache-v1";
+const CACHE_NAME = "bucci-v1";
 
 const urlsToCache = [
   "/",
+  "index",
+  "/portfolio",
+  "/blog",
+  "/blogsingle",
   "/buccimain.png",
   "/css/style.css",
   "/js/js.js",
   "/js/formapi.js", 
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  console.log("[Service Worker] Installing...");
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("[Service Worker] Activated");
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
   );
 });
+
